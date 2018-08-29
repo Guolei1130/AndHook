a50516930c031ba26b1cef4f052be6700e636d13
guolei1130 <1120832563@qq.com>
2018-08-29 08:34:38 +0800

    能debug

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI          := armeabi-v7a arm64-v8a x86 x86_64
-APP_OPTIM        := release
+APP_OPTIM        := debug
 APP_PLATFORM     := android-27
 APP_STL          := system
 APP_CPPFLAGS     := -std=c++1y
